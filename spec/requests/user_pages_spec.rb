1b47648ffad06615e3ae33d70d8a4f6874bedf4c
megamaksimsPC <maksimpanarin88@gmail.com>
2018-02-25 17:40:16 +0200

    Finish user edit, update, index, and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -2,11 +2,14 @@ require 'spec_helper'
 
 describe "User pages" do
 
+
   subject { page }
 
   describe "index" do
+
     let(:user) { FactoryGirl.create(:user) }
-    before(:each) do
+
+    before do
       sign_in user
       visit users_path
     end
@@ -28,11 +31,32 @@ describe "User pages" do
       end
     end
 
-    it "should list each user" do
-      User.all.each do |user|
-        expect(page).to have_selector('li', text: user.name)
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
       end
     end
+
+      it "should list each user" do
+        User.all.each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
   end
 
   describe "profile page" do
