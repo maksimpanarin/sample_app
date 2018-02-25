21bccd70cd02e869f0d182b88cd32cd081b6fa6b
megamaksimsPC <maksimpanarin88@gmail.com>
2018-02-25 21:15:48 +0200

    10.3.4 Completed - destroy micropost action added

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
   before_action :signed_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def index
   end
@@ -8,7 +9,7 @@ class MicropostsController < ApplicationController
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
       flash[:success] = "Micropost created!"
-      redirect_to root_url
+      redirect_to root_path
     else
       @feed_items = []
       render 'static_pages/home'
@@ -16,11 +17,18 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to root_path
   end
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to root_path if @micropost.nil?
   end
 end
